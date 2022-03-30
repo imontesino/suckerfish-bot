d79d127891f7a7965905c5a80fd345aa58eada71
Ignacio Montesino <monte.igna@gmail.com>
2022-03-31 01:13:16 +0200

    Add dev chat

--- bot_main.py
+++ bot_main.py
@@ -1,6 +1,8 @@
 from asyncio.log import logger
 import os
 
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 from bot.suckerfish_bot import SuckerfishBot
 from utils.loggers import DevChatLogger
 from utils.config import get_config
@@ -14,34 +16,45 @@ def main():
     bot_config, log_config = get_config('config.yaml')
 
     logger = DevChatLogger(
-        log_config['dev_bot_token'],
+        dev_updater,
         log_config['dev_chat_id'],
         log_config['log_file'],
         log_config['chat_log_level'],
         log_config['file_log_level']
     )
 
-    bot = SuckerfishBot(
-        bot_config['bot_token'],
-        bot_config['host_ip'],
-        bot_config['host_username'],
-        bot_config['power_pin'],
-        bot_config['reset_pin'],
-        bot_config['windows_entry_id'],
-        bot_config['allowed_chats'],
-        logger
-    )
+    try:
+        bot = SuckerfishBot(
+            bot_config['bot_token'],
+            bot_config['host_ip'],
+            bot_config['host_username'],
+            bot_config['power_pin'],
+            bot_config['reset_pin'],
+            bot_config['windows_entry_id'],
+            bot_config['allowed_chats'],
+            logger
+        )
 
-    # Start the bot
-    bot.start()
+        # Start the bot
+        bot.start()
 
-    # Start the bot
-    bot.start()
+        # Run the bot until you press Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT. This should be used most of the time, since
+        # start() is non-blocking and will stop the bot gracefully.
+        bot.idle()
+    except KeyboardInterrupt:
+        logger.info('KeyboardInterrupt received, exiting...')
+    except Exception as e:
+        # Create the dev chat bot
+        dev_updater = Updater(token=bot_config['dev_bot_token'])
+        dev_bot = dev_updater.bot
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start() is non-blocking and will stop the bot gracefully.
-    bot.idle()
+        # Send the error to the dev chat
+        dev_bot.send_message(
+            chat_id=log_config['dev_chat_id'],
+            text=f'Error: {e}'
+        )
+
 
 if __name__ == "__main__":
     main()
